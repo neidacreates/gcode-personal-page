84163ed3dea6ec784d966cada2d4b94a8b3d590d
Neida <84603848+neidacreates@users.noreply.github.com>
2022-03-05 13:57:20 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,154 +1,71 @@
 console.log('JS file is connected');
 
-//function for home page button
-
 function toggleDarkMode(){
     var bodyElement = document.getElementById("main-background"); 
     var darkModeButtonElement = document.getElementById("dark-mode-btn");
     var menuBar = document.getElementById("menu-bar");
-    var listPicBorders = document.getElementsByClassName("list-pics"); //returns an array
     var menuLinks = document.getElementsByClassName("menu-links"); //returns an array
-    var outsideLinks = document.getElementsByClassName("outside-web-links"); //returns an array
-    var plantListHeading = document.getElementById("plant-list-heading");
-    var homepagePic = document.getElementById("homepage-pic");
+    var otherLinks = document.getElementsByClassName("other-links"); //returns an array
+    var allPictures = document.getElementsByClassName("pictures"); //returns an array
     
-
+//going from white background to green background, green text to white text
     if (bodyElement.style.backgroundColor === "white"){
         console.log('we go into the if statement block and switch to dark mode');
+        //changing background color
+        bodyElement.style.backgroundColor = "rgb(30,53,10)";
+        //changing body text color
+        bodyElement.style.color = "white";
+        //changing menu bar background color and text color
+        menuBar.style.backgroundColor = "white";
+        menuBar.style.color = "rgb(30,53,10)";
+        //changing all links color
         for (i=0;i<menuLinks.length;i++) {
             menuLinks.item(i).classList.remove("light");
-        }
-        //changing background color 
-        bodyElement.style.backgroundColor = "rgb(42, 97, 55)";
-        //changing body text color 
-        bodyElement.style.color = "white";
-        plantListHeading.style.color = "white";
-        //changing menu bar background color and text color 
-        menuBar.style.backgroundColor = "white";
-        menuBar.style.color = "rgb(42, 97, 55)";
-        //changing links color 
-
-        for (i=0;i<menuLinks.length;i++) {
             menuLinks.item(i).classList.add("dark");
         }
-        for (i=0;i<outsideLinks.length;i++) {
-            outsideLinks.item(i).style.color = "white";
+        for (i=0;i<otherLinks.length;i++) {
+            otherLinks.item(i).classList.remove("light");
+            otherLinks.item(i).classList.add("dark");
         }
-
-        //changing color of borders around pictures 
-        for (i=0;i<listPicBorders.length;i++) {
-            listPicBorders.item(i).style.borderColor = "white";
+        //changing color of borders around pictures
+        for (i=0;i<allPictures.length;i++) {
+            allPictures.item(i).style.borderColor = "white";
         }
-        homepagePic.style.borderColor ="white";
-        //changing button text 
+        //changing button text
         darkModeButtonElement.innerHTML = "Switch to Light Mode";
         //changing button color
         darkModeButtonElement.style.backgroundColor = "white";
-        darkModeButtonElement.style.color = "rgb(42, 97, 55)";
+        darkModeButtonElement.style.color = "rgb(30,53,10)";
     } 
+//going from green background to white background, white text to green text
     else {
         console.log('we go into the else statement block and switch to light mode');
         //changing background color 
         bodyElement.style.backgroundColor = "white"; 
-        //changing body text color 
-        bodyElement.style.color = "rgb(42, 97, 55)";
-        plantListHeading.style.color = "rgb(42, 97, 55)";
-        //changing menu bar background color and text color 
-        menuBar.style.backgroundColor = "rgb(42, 97, 55)";
+        //changing body text color
+        bodyElement.style.color = "rgb(30,53,10)";
+        //changing menu bar background color and text color
+        menuBar.style.backgroundColor = "rgb(30,53,10)";
         menuBar.style.color = "white";
         //changing links color 
         for (i=0;i<menuLinks.length;i++) {
+            menuLinks.item(i).classList.remove("dark");
             menuLinks.item(i).classList.add("light");
         }
-        for (i=0;i<outsideLinks.length;i++) {
-            outsideLinks.item(i).style.color = "rgb(42, 97, 55)";
+        for (i=0;i<otherLinks.length;i++) {
+            otherLinks.item(i).classList.remove("dark");
+            otherLinks.item(i).classList.add("light");
         }
-        //changing color of borders around pictures 
-        for (i=0;i<listPicBorders.length;i++) {
-            listPicBorders.item(i).style.borderColor = "rgb(42, 97, 55)";
+        //changing color of borders around pictures
+        for (i=0;i<allPictures.length;i++) {
+            allPictures.item(i).style.borderColor = "rgb(30,53,10)";
         }
-        homepagePic.style.borderColor ="rgb(42, 97, 55)";
         //changing button text 
         darkModeButtonElement.innerHTML = "Switch to Dark Mode";
         //changing button color
-        darkModeButtonElement.style.backgroundColor = "rgb(42, 97, 55)";
+        darkModeButtonElement.style.backgroundColor = "rgb(30,53,10)";
         darkModeButtonElement.style.color = "white";
 
-    }
-    
-    
-};
-
-//function for the about page button
-
-function toggleDarkModeAbout(){
-    var bodyElement = document.getElementById("main-background"); 
-    var darkModeButtonElement = document.getElementById("dark-mode-btn");
-    var menuBar = document.getElementById("menu-bar");
-    var menuLinks = document.getElementsByClassName("menu-links"); //returns an array
-    var mainPic = document.getElementById("about-picture");
-    
-
-    if (bodyElement.style.backgroundColor === "white"){
-        console.log('we go into the if statement block and switch to dark mode');
-        for (i=0;i<menuLinks.length;i++) {
-            menuLinks.item(i).classList.remove("light");
-        }
-        //changing background color 
-        bodyElement.style.backgroundColor = "rgb(42, 97, 55)";
-        
-        //changing body text color 
-        bodyElement.style.color = "white";
-        
-        //changing menu bar background color and text color 
-        menuBar.style.backgroundColor = "white";
-        menuBar.style.color = "rgb(42, 97, 55)";
-      
-        //changing links color
-        for (i=0;i<menuLinks.length;i++) {
-            menuLinks.item(i).classList.add("dark");
-        }
-        
-        mainPic.style.borderColor ="white";
-      
-        //changing button text
-        darkModeButtonElement.innerHTML = "Switch to Light Mode";
-      
-        //changing button color
-        darkModeButtonElement.style.backgroundColor = "white";
-        darkModeButtonElement.style.color = "rgb(42, 97, 55)";
     } 
-    else {
-        console.log('we go into the else statement block and switch to light mode');
-        //changing background color
-        bodyElement.style.backgroundColor = "white";
-      
-        //changing body text color
-        bodyElement.style.color = "rgb(42, 97, 55)";
-        
-        //changing menu bar background color and text color
-        menuBar.style.backgroundColor = "rgb(42, 97, 55)";
-        menuBar.style.color = "white";
-      
-        //changing links color
-        for (i=0;i<menuLinks.length;i++) {
-            menuLinks.item(i).classList.add("light");
-        }
-        
-        //changing color of borders around pictures
-        mainPic.style.borderColor ="rgb(42, 97, 55)";
-      
-        //changing button text
-        darkModeButtonElement.innerHTML = "Switch to Dark Mode";
-      
-        //changing button color
-        darkModeButtonElement.style.backgroundColor = "rgb(42, 97, 55)";
-        darkModeButtonElement.style.color = "white";
-
-    }
-    
-    
 };
-
-
 
